462846a84438f80faec724eb8ce94e4bc84bc1fc
AzerJradi <azerjradi13@gmail.com>
2024-02-25 01:58:38 +0100

    project

---
+++ src/UserList.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './List.css'
+
+function UserList() {
+    const [listOfUsers, setListOfUsers] = useState([]);
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                setListOfUsers(response.data);
+            })
+    }, []);
+    return (
+        <div className='list'>
+            <h1>User List</h1>
+            <ul>
+                {listOfUsers.map(user => (
+                    <li key={user.id}>
+                        {user.name} - {user.email} - {user.phone}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default UserList;
